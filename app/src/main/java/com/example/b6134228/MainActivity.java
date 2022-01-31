9cbf34ee679d4525d2f20dfa709895702a452d4b
Pharadorn Boonruam <61776315+pharadornb@users.noreply.github.com>
2022-01-31 12:38:44 +0700

    Update Lab01 31/01/2565 12.37 - Change th/en - Swap portrait and horizontal - Toast and switch event

--- app/src/main/java/com/example/b6134228/MainActivity.java
+++ app/src/main/java/com/example/b6134228/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.b6134228;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSwitchLight1(View v){
+        boolean on = ((Switch) v).isChecked();
+        if(on)
+        {
+            Toast.makeText(getApplicationContext(),R.string.toast_on_light1, Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),R.string.toast_off_light1, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onSwitchLight2(View v){
+        boolean on = ((Switch) v).isChecked();
+        if(on)
+        {
+            Toast.makeText(getApplicationContext(),R.string.toast_on_light2, Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),R.string.toast_off_light2, Toast.LENGTH_SHORT).show();
+        }
     }
 }
